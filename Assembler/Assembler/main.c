5a1a99c938c7707c2692b91a71c4963fcfeec8ce
Amnon Ophir <ophiramnon@gmail.com>
2018-11-28 18:19:46 +0200

    Labels arangement
    labels are now choped and stored properly in linked list

--- Assembler/Assembler/main.c
+++ Assembler/Assembler/main.c
@@ -2,27 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
+//### Max lablel length: 50 , Max commandline: 500 therfore: ###
 #define TRUE 1
 #define FALSE 0
 #define MAX_LENGTH 501
-#define MAX_LABLE_LENGTH 51
+#define MAX_LABEL_LENGTH 51
 
 typedef struct label
 {
-	char Name[MAX_LABLE_LENGTH];
+	char Name[MAX_LABEL_LENGTH];
 	int Line;
 	struct label *next;
 } label_t;
 
+int findIndex(char *string, int size, char target)
+{
+	int i = 0;
+	while ((i < size) && (string[i] != target)) i++;
+
+	return (i < size) ? (i) : (-1);
+}
+
 label_t* addLable(label_t *head, char* assemblyToken, int tokenLength, int locationCounter)
 {
-	label_t *newLabel = (label_t*)malloc(sizeof(label_t));
-	if (newLabel == NULL)
-	{
+	char tempString[MAX_LABEL_LENGTH];
+	label_t *newLabel = (label_t*)malloc(sizeof(label_t));		//allocation
+	char target;
+	int targetIndex = 0;
+	if (newLabel == NULL)	//exception//
 		return 1;
-	}
-	//assemblyToken[-1] = '\0';
-	strcpy(newLabel->Name, assemblyToken);
+	targetIndex = findIndex(assemblyToken, tokenLength, ':');	//index of ':'
+	strcpy(tempString, assemblyToken);
+	tempString[targetIndex] = '\0';		//removes ':'
+	strcpy(newLabel->Name, tempString);		//construct//
 	newLabel->Line = locationCounter;
 	newLabel->next = head;
 	return newLabel;
@@ -30,8 +42,8 @@ label_t* addLable(label_t *head, char* assemblyToken, int tokenLength, int locat
 
 void firstPass(char* fileName , label_t* labels )
 {
+	// marks each label and keeps their adress as instructed//
 	int num;
-	//char newstring[500] = "";
 	char assemblyLine[500] ;
 	char *assemblyToken;
 	int locationCounter = 0;
@@ -42,20 +54,16 @@ void firstPass(char* fileName , label_t* labels )
 		printf("Error! opening file");
 		exit(1);
 	}
-	while (fgets(assemblyLine, MAX_LENGTH, fptr))
+	while (fgets(assemblyLine, MAX_LENGTH, fptr))		//while not EOF
 	{
-		//assemblyLine[0] = '\0';//memset
-		//fgets(assemblyLine, MAX_LENGTH, fptr); //this one is corrupting
+		locationCounter++;		//new assembly line
 		assemblyToken = strtok(assemblyLine, " \t");
-
-		if (assemblyToken == NULL) //beacause an exaption was thrown otherwise
+		if (assemblyToken == NULL)		//beacause an exaption was thrown otherwise
 			break;
-		// Check line is not a comment
-		if (assemblyToken[0] != '#')
+		if (assemblyToken[0] != '#') 		// Check line is not a comment
 		{
-			// Is this a label
-			tokenLength = strlen(assemblyToken);
-			if (assemblyToken[tokenLength - 1] == ':')
+			tokenLength = strlen(assemblyToken);		//checks if it's a Label
+			if( (assemblyToken[tokenLength - 1] == ':')||(assemblyToken[tokenLength - 2] == ':'))
 			{
 				// If it is a label record it and location counter
 				// TODO: Check the label does not exist
@@ -66,11 +74,6 @@ void firstPass(char* fileName , label_t* labels )
 			{
 				containsEndCommand = TRUE;
 			}
-			if (assemblyToken == NULL)
-			{
-				break;
-			}
-			locationCounter++;
 		}
 	}
 
