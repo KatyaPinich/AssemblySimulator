a08911fed090407b6c226fb25dfbf402f8d31940
Katya Feldman <katyapinich@gmail.com>
2018-12-01 12:09:06 +0200

    Started runSimulator function

--- Assembler/Simulator/Main.c
+++ Assembler/Simulator/Main.c
@@ -1,4 +1,22 @@
+#include <stdio.h>
+
+#define NUM_OF_REGISTERS 16
+
+void runSimulator(char* inMemoryFilename)
+{
+	int registers[NUM_OF_REGISTERS];
+	for (int i = 0; i < NUM_OF_REGISTERS; i++)
+	{
+		registers[i] = 0;
+	}
+
+
+	// Fetch
+	// Decode
+	// Execute
+}
+
 int main(int argc, char *args[])
 {
-
+	char* inMemoryFilename = args[1];
 }
